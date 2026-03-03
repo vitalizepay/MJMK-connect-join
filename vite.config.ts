83eedf9b27c3fd1983bae4376518893e1aa26c07
vitalizepay <vitalizepay@gmail.com>
2026-03-03 17:39:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/tamil-connect-join/" : "/",
+  base: mode === "production" ? "/MJMK-connect-join/" : "/",
   
   server: {
     host: "::",
