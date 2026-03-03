9931c8d6481536c3ed7dccf034edb48f5114409a
vitalizepay <vitalizepay@gmail.com>
2026-03-03 17:48:32 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={import.meta.env.MODE === "production" ? "/MJMK-connect-join" : ""}>
+      <BrowserRouter basename="/tamil-connect-join">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
